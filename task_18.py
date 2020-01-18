a6050876378585a67f8a79870c9a6d2244b2bf28
koliadaKol <kolyada_yv@edu.kvantorium-pk.ru>
2020-01-18 20:12:47 +1000

    task17

--- task_18.py
+++ task_18.py
@@ -5,6 +5,20 @@ from pyrob.api import *
 
 @task
 def task_8_28():
+    while wall_is_above():
+        while not wall_is_on_the_right():
+            move_right()
+        if not wall_is_above():
+            move_up()
+            while not wall_is_on_the_left():
+                move_left()
+        if  wall_is_on_the_right():
+            while not wall_is_on_the_left():
+                move_left()
+    if wall_is_above():
+        move_up()
+        while not wall_is_on_the_left():
+            move_left()
     pass
 
 

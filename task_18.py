1be0b3a3941f14544419fb9962c5c02e2578bc24
koliadaKol <koliadayaroslav2003@gmail.com>
2020-01-26 17:26:42 +1000

    need

--- task_18.py
+++ task_18.py
@@ -3,50 +3,21 @@
 from pyrob.api import *
 
 
-@task
+@task(delay=0.05)
 def task_8_28():
-<<<<<<< HEAD
+    while not wall_is_on_the_left():
+        move_left()
     while wall_is_above():
         while not wall_is_on_the_right():
             move_right()
-        if not wall_is_above():
-            move_up()
-            while not wall_is_on_the_left():
-                move_left()
-        if  wall_is_on_the_right():
-            while not wall_is_on_the_left():
-                move_left()
-    if wall_is_above():
-        move_up()
-        while not wall_is_on_the_left():
-            move_left()
-=======
-    if not wall_is_above():
-        while not wall_is_above():
-            move_up()
-        while not wall_is_on_the_left():
-            move_left()
-        return
-    while not wall_is_on_the_right():
-        if wall_is_above():
             while not wall_is_above():
                 move_up()
-            while not wall_is_on_the_left():
-                move_left()
-            return
-        move_right()
-        if wall_is_on_the_right():
-            while not wall_is_on_the_left():
-                if not wall_is_above():
-                    while not wall_is_above():
-                        move_up()
+                if wall_is_above():
                     while not wall_is_on_the_left():
                         move_left()
-                    return
-                move_left()
->>>>>>> 1f06268b01a3b9569a7cb3064ddb6b3e62a04c76
+                        if wall_is_on_the_left() and wall_is_above():
+                            return
     pass
-
 
 if __name__ == '__main__':
     run_tasks()
